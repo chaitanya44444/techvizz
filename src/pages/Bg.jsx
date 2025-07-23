4e1e81833b840388358625a806d0c3a0ff91c258
Chaitanya Vaswani <93338022+chaitanya44444@users.noreply.github.com>
2025-07-23 21:06:45 +0530

    Update Bg.jsx

--- src/pages/Bg.jsx
+++ src/pages/Bg.jsx
@@ -5,12 +5,12 @@ const Bg = ({ children }) => {
   const location = useLocation();
   const path = location.pathname;//stckoverflow for the W :sob: didnt have to thinkof it
 
-  let background;
+  let bg;
 
   if (path === '/' ) {
-    background = '#14142D';
+    bg = '#14142D';
   } else{
-    background ='radial-gradient(ellipse at top, #FEAD13 20%, #14142D 60%, #ffffff 100%)'; 
+    bg = '#14142D';
   } 
 
   return (
